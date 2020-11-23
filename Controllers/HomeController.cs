c011f3f9e2613f5b024a6b5295f1b2d820b132d5
Leger <Legerumia@gmail.com>
2020-11-24 00:21:56 +0300

    Main page with books

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using Shop.Data.Interfaces;
+using Shop.ViewModels;
+
+namespace Shop.Controllers
+{
+    public class HomeController:Controller
+    {
+        private readonly IAllBooks _bookRepository;
+
+        public HomeController(IAllBooks bookRepository)
+        {
+            _bookRepository = bookRepository;
+        }
+
+        public ViewResult Index()
+        {
+            var homeBooks = new HomeViewModel
+            {
+                MainBooks = _bookRepository.GetMainBooks
+            };
+            return View(homeBooks);
+        }
+    }
+}
---
+++ ViewModels/HomeViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Shop.Data.Models;
+
+namespace Shop.ViewModels
+{
+    public class HomeViewModel
+    {
+        public IEnumerable<Book> MainBooks { set; get; }
+    }
+}
